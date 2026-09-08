3fed3ebe78012d7015aa98f8ff2fc594addfc3cf
andy-and-terry <248389669+andy-and-terry@users.noreply.github.com>
2026-09-08 20:10:16 +0800

    Create red.js

---
+++ tools/scratchedit-src/src/lib/themes/accent/red.js
@@ -0,0 +1,28 @@
+//modified to blue
+const guiColors = {
+    'motion-primary': '#4c8cff',
+    'motion-primary-transparent': '#4c8cffe6',
+    'motion-tertiary': '#3366cc',
+
+    'looks-secondary': '#4c8cff',
+    'looks-transparent': '#4d8dff59',
+    'looks-light-transparent': '#4d8dff26',
+    'looks-secondary-dark': 'hsla(220, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(200, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(200, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(200, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(200, 57%, 85%, 1)',
+
+    'drop-highlight': '#8cb8ff'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#4c8cff',
+    checkboxActiveBorder: '#3366cc'
+};
+
+export {
+    guiColors,
+    blockColors
+};
